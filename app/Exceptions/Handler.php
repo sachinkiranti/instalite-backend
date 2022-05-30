2c9ba765c00751f0979ef1eb68e2e808bf6825eb
Sachin Rai <sachinkiranti@gmail.com>
2022-05-30 22:26:22 +0545

    feat: add custom exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,21 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 use Throwable;
+use Illuminate\Http\Request;
+use Foundation\Mixins\HasApiResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
+
+    use HasApiResponse;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -37,5 +47,44 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+
+        $this->renderable(function (Throwable $e, Request $request) {
+            return $this->handleExceptions($e, $request);
+        });
+    }
+
+    private function handleExceptions(Throwable $e, Request $request)
+    {
+        if (
+            $e instanceof ModelNotFoundException ||
+            $e instanceof NotFoundHttpException
+        ) {
+            return $this->responseError(
+                null,
+                Response::HTTP_NOT_FOUND,
+                'Not Found !'. $e->getMessage(),
+                null
+            );
+        }
+
+        if ( $e instanceof MethodNotAllowedHttpException ) {
+            return $this->responseError(
+                null,
+                Response::HTTP_NOT_FOUND,
+                'Method Not Found !'. $e->getMessage(),
+                null
+            );
+        }
+
+        if($e instanceof AuthenticationException ){
+
+            return $this->responseError(
+                null,
+                Response::HTTP_UNAUTHORIZED,
+                'The request is unauthorized.',
+                'unauthorized'
+            );
+
+        }
     }
 }
