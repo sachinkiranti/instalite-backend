1cce2bce723293f09ced86b4bc559e11a5890345
Sachin Rai <sachinkiranti@gmail.com>
2022-06-02 23:21:33 +0545

    feat: add reactions

---
+++ database/migrations/2022_06_02_172740_create_reactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Foundation\Enums\Reaction;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained();
+            $table->tinyInteger('reaction')->default( Reaction::REACTION_LIKE );
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reactions');
+    }
+}
