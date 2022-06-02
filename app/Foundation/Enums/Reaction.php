42541d93ef03df00c8f2c89958bf6645294de760
Sachin Rai <sachinkiranti@gmail.com>
2022-06-02 23:20:26 +0545

    feat: add constants for reaction

---
+++ app/Foundation/Enums/Reaction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Foundation\Enums;
+
+class Reaction
+{
+
+    const REACTION_LIKE  = 0;
+    const REACTION_LOVE  = 1;
+    const REACTION_LAUGH = 2;
+    const REACTION_SAD   = 3;
+
+}
